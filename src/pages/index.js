e9bc2c097533da07cea0f868728c86d3008c3693
Windsom <86801382+wiindsom@users.noreply.github.com>
2025-05-06 15:20:55 -0700

    update

---
+++ src/pages/index.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import Head from '@docusaurus/Head';
+
+export default function Home() {
+  return (
+    <>
+      <Head>
+        <title>Structix</title>
+        <meta property="og:title" content="Structix" />
+        <meta property="og:site_name" content="wiindsom" />
+        <meta name="theme-color" content="#B19CD7" />
+      </Head>
+      <main>
+        <h1 style={{ display: 'none' }}>Structix</h1>
+      </main>
+    </>
+  );
+}
