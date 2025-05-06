ee51843c26b4d0bbc93cf7e33edeaea98117d640
Windsom <86801382+wiindsom@users.noreply.github.com>
2025-05-06 15:26:36 -0700

    Update index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,18 +1,20 @@
 import React from 'react';
 import Head from '@docusaurus/Head';
+import Layout from '@theme/Layout';
 
 export default function Home() {
   return (
-    <>
+    <Layout title="">
       <Head>
         <title>Structix</title>
         <meta property="og:title" content="Structix" />
         <meta property="og:site_name" content="wiindsom" />
         <meta name="theme-color" content="#B19CD7" />
+        <meta name="description" content="A data structure library for Roblox developers." />
       </Head>
       <main>
         <h1 style={{ display: 'none' }}>Structix</h1>
       </main>
-    </>
+    </Layout>
   );
 }
