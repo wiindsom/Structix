3f353cfbd9a73a81be0bce8fc22827070808a080
Windsom <86801382+wiindsom@users.noreply.github.com>
2025-05-06 15:30:55 -0700

    a

--- pages/index.js
+++ pages/index.js
@@ -1,20 +1,21 @@
 import React from 'react';
 import Head from '@docusaurus/Head';
-import Layout from '@theme/Layout';
 
 export default function Home() {
   return (
-    <Layout title="">
+    <>
       <Head>
         <title>Structix</title>
         <meta property="og:title" content="Structix" />
+        <meta property="og:description" content="A data structure library for Roblox developers." />
+        <meta property="og:image" content="https://wiindsom.github.io/Structix/og-image.jpg" />
         <meta property="og:site_name" content="wiindsom" />
         <meta name="theme-color" content="#B19CD7" />
-        <meta name="description" content="A data structure library for Roblox developers." />
+        <meta name="twitter:card" content="summary_large_image" />
       </Head>
       <main>
         <h1 style={{ display: 'none' }}>Structix</h1>
       </main>
-    </Layout>
+    </>
   );
 }
