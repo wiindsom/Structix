090c2c38b17dac88f71cd392d238ef8860cae64e
Windsom <86801382+wiindsom@users.noreply.github.com>
2025-05-06 21:08:04 -0700

    Built and published by Moonwave

---
+++ assets/js/14608923.9474fa66.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunkdocs=self.webpackChunkdocs||[]).push([[410],{4410:s=>{s.exports=JSON.parse('{"sourceUrl":"https://github.com/wiindsom/Structix/blob/main","baseUrl":"/Structix/","classOrder":[],"apiCategories":[]}')}}]);
